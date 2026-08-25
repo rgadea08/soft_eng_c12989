f8c408396e5349d224a0a153d7c82a95c778a0ef
Raul Gadea Alfaro <173732489+SisyphusGrind@users.noreply.github.com>
2026-08-24 21:36:42 -0600

    HW2 Tabs sections

--- homeworks/HW2/js/scripts.js
+++ homeworks/HW2/js/scripts.js
@@ -3,3 +3,15 @@ const darkModeToggle = document.getElementById('darkModeToggle');
 darkModeToggle.addEventListener('change', function() {
   document.body.classList.toggle('dark-mode');
 });
+
+function activateTab(tabId) {
+  document.querySelectorAll('.tab-content').forEach(function(t) {
+    t.classList.remove('active');
+  });
+  document.querySelectorAll('.tab-btn').forEach(function(b) {
+    b.classList.remove('active');
+  });
+
+  document.getElementById(tabId).classList.add('active');
+  document.querySelector(`.tab-btn[data-tab="${tabId}"]`).classList.add('active');
+}
