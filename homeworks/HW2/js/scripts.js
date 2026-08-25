da2739897ddd30b0d39725f2aaf5a984f5d45c25
Raul Gadea Alfaro <173732489+SisyphusGrind@users.noreply.github.com>
2026-08-24 21:44:00 -0600

    HW2 Dropdown text

--- homeworks/HW2/js/scripts.js
+++ homeworks/HW2/js/scripts.js
@@ -15,3 +15,14 @@ function activateTab(tabId) {
   document.getElementById(tabId).classList.add('active');
   document.querySelector(`.tab-btn[data-tab="${tabId}"]`).classList.add('active');
 }
+
+function toggleQuote(quoteId, button) {
+  const quote = document.getElementById(quoteId);
+  quote.classList.toggle('expanded');
+
+  if (quote.classList.contains('expanded')) {
+    button.textContent = 'Read less';
+  } else {
+    button.textContent = 'Read more';
+  }
+}
